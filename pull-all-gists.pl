d955b9e62da34e0ecaf42c9e16d79c2a1ee60ccc
Richard Russon <rich@flatcap.org>
2015-04-10 23:42:29 +0100

    list all gists

--- pull-all-gists.pl
+++ pull-all-gists.pl
@@ -8,15 +8,78 @@ our $VERSION = 0.1;
 use LWP::Simple;
 use JSON qw(decode_json);
 use Data::Dumper;
+use Readonly;
 
-# my $url = "https://api.github.com/users/flatcap/gists";
-my $url = 'http://flatcap.xyz/gists/flatcap.json';
+Readonly my $DATE_LEN  => 10;
+Readonly my $MAX_PAGES => 20;
 
-my $json = get ($url);
-if (!defined $json) {
-	die "Could not get $url!";
+sub get_gist_page
+{
+	my ($user, $page) = @_;
+
+	my $url = sprintf 'https://api.github.com/users/%s/gists?page=%d', $user, $page;
+
+	my $json_str = get ($url);
+	if (!defined $json_str) {
+		printf "Could not get $url";
+		return;
+	}
+
+	my $obj = decode_json ($json_str);
+
+	return $obj;
 }
 
-my $decoded_json = decode_json ($json);
-print Dumper $decoded_json;
+sub get_all_gists
+{
+	my ($user) = @_;
+	my @gists;
+
+	for my $i (1 .. $MAX_PAGES) {
+		my @pages = get_gist_page ($user, $i);
+		if (!@pages) {
+			return;
+		}
+
+		my $count = scalar keys $pages[0];
+		if ($count == 0) {
+			last;
+		}
+
+		foreach my $g (keys $pages[0]) {
+			# printf "%s\n", $pages[0][$g]->{'id'};
+			push @gists, $pages[0][$g];
+		}
+	}
+
+	return @gists;
+}
+
+sub main
+{
+	my $user = $ARGV[0] || 'flatcap';
+
+	my @gists = get_all_gists ($user);
+	if (!@gists) {
+		return;
+	}
+
+	for my $g (@gists) {
+		my $date = substr $g->{'created_at'}, 0, $DATE_LEN;
+		printf "%s - %s\n", $date, $g->{'id'};
+		my $d = $g->{'description'};
+		if ($d) {
+			printf "\t\"%s\"\n", $d;
+		}
+		for my $f (keys $g->{'files'}) {
+			printf "\t%s (%d bytes)\n", $f, $g->{'files'}->{$f}->{'size'};
+		}
+		printf "\n";
+	}
+
+	return;
+}
+
+
+main ();
 
